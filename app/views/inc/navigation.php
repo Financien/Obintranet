039c2805e6853c28c9158c0f2181edceb8f83742
Gerrit <gerrit.soerotaroeno@finance.gov.sr>
2018-11-12 11:34:25 -0300

    header img and img upload

--- app/views/inc/navigation.php
+++ app/views/inc/navigation.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
         <a class="navbar-brand" href="#">
-          <img src="https://via.placeholder.com/70x50" alt="">
+          <img class= "headerimg" src="./public/img/wapensuriname.png" alt="">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
