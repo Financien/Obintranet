0f169a43d7c9511f03f0bd80b4d669a58da4461d
timmy1420 <tim.poco01@gmail.com>
2018-12-18 10:42:47 -0300

    Updated gitignore

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,7 @@
 //DB Params
 define("DB_HOST", "localhost");
 define("DB_USER", "root");
-define("DB_PASS", "timpocovalen");
+define("DB_PASS", "");
 define("DB_NAME", "obintranet");
 define("DB_DRIVER", "mysql");
 
