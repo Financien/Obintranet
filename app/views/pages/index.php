f820a8f566d2553748ee05ce937e179789a20c2d
Gerrit <gerrit.soerotaroeno@finance.gov.sr>
2018-12-21 10:55:26 -0300

    minor changes,site header text and css file upgedate

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,4 +1,6 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
+
+<h1 class="SiteName"> Intranet Minsterie van Financien </h1>
 <?php require APPROOT . '/views/pages/slider.php'; ?>
 
   <!-- </div>
--- app/views/pages/personeelbond.php
+++ app/views/pages/personeelbond.php
@@ -7,7 +7,7 @@ require APPROOT . '/views/inc/header.php';
     <section class="mt-3">
         <div class="row">
             <div class="col ">
-                <h2 class="text-center">Bond Personeel Dienst der belastingen</h2>
+                <h2 class="text-center"><strong>Bond Personeel Dienst der belastingen</strong></h2>
                 <hr>
                 <p>Het behoefte geen betoog dat heden ten dage communicatie, in al haar facetten, onontbeerlijk is. In
                     dat kader zie ik daarom het verschijnen van dit interne kwartaalblad. In algemene zin zal het doel
@@ -22,6 +22,82 @@ require APPROOT . '/views/inc/header.php';
                     dit interne kwartaalblad de verwachte doelstellingen zullen worden bereikt</p>
             </div>
         </div>
+    </section>
+    <hr>
+    <h4 class="text-center"><strong>Samenstelling van de Personeelsbonden</strong></h4>
+    <section class="mt-5">
+    <div class="row">
+  <div class="col-sm-6">
+    
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title"><strong>Bond Technisch Personeel Directe Belastingen</strong></h5>
+        <hr>
+        <p class="card-text">Vanaf  22 juni 2018 is er een nieuw gekozen Vakbondsbestuur bij de
+            Dienst der Belastingen. De samenstelling ziet er als volgt uit:</p>
+        <hr>
+        <div class="text-center">
+        <p>Dhr. Woon Iwan ( voorzitter )</p>
+        <p>Dhr. Plet, H.</p>
+        <p>Mw. Sastroredjo, A.</p>
+        <p>Mw. Bijron, N.</p>
+        <p>Dhr. Pansa, R.</p>
+        <p>Dhr. Hossenbux, A.</p>
+        <p>Mw. Alladien, N.</p>
+        <p>Mw. Chaitsing, R.</p>
+        <p>Dhr. Darson, R.</p>
+        <p>Dhr. Gadjradj, R.</p>
+        <p>Dhr. Pahladsingh, R.</p>
+        <p>Dhr. Abudin, T.</p>
+        <p>Dhr. Dors, M.</p>
+        <p>Mw. Alimoenadi, L.</p>
+        <hr>
+                <div class="text-left">
+                <span><i class="fas fa-users"></i> Contact </span>
+                <br>
+                <span><i class="fas fa-phone"></i> Telefoonnr. : 477838 </span>
+                <br>
+                <span><i class="fa fa-envelope"></i> Email. : bondbelastingdienst@gmail.com <span>
+                </div>
+            
+        </div>
+        <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+         <div class="card-body">
+         <h5 class="card-title"><strong>Bond van Ambtenaren bij de Inspectie der Invoerrechten en Accijnzen</strong></h5>
+        <hr>
+        <img class="card-img-top" src="<?= URLROOT ?>/img/Personeels_douna_azijnswet.png">
+        <hr>
+        <p class="card-text">Vanaf 29 augustus 2018 is er een nieuw bestuur bij   acclamatie gekozen voor de Douane. De samenstelling ziet er als volgt uit:</p>
+        <br>
+        <div class="text-center">
+                <p>Joyce de Vlugt  (Voorzitter)</p>
+                    <p>Remy Belliot  (Ondervoorzitter)</p>
+                        <p>Urvin Blomhof  (1e Penningmeester)</p>
+                            <p>John Valies ( 2e Penningmeester)</p>
+                                <p>Rachel Breeveld  (Secretaris Generaal)</p>
+                          <p>Graciella Hovel  (1e Secretaris</p>
+                      <p>Vyay Shiamrai  (2e Secretaris)</p>
+                   <p>Rignon Gambier  (1e Commissaris)</p>
+               <p>Winston Liefden  (2e Commissaris)</p>
+               <hr>
+                <div class="text-left">
+                <span><i class="fas fa-users"></i> Contact </span>
+                <br>
+                <span><i class="fas fa-phone"></i> Telefoonnr. :  813227 /8616126</span>
+                <br>
+                <span><i class="fa fa-envelope"></i> Email. : douanebond@hotmail.com  <span>
+                </div>
+        </div>
+        <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+      </div>
+    </div>
+  </div>
+</div>
     </section>
 </div>
 <?php
